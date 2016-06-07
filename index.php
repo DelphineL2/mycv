82ede6862103e2e2b45a5f7de1b90bf0ac7b5d82
Delphine LEMIRE <delphine.lemire@gmail.com>
2016-06-07 12:14:46 +0200

    Mise en place

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Delphine Lemire, analyste développeur nouvelles technologies" />
+		<meta property="og:title" content="Delphine Lemire, Analyste Développeur" />
+		<meta property="og:type" content="Informatique" />
+		<meta property="og:url" content="http://delphinelemire.fr/" />
+		<meta property="og:image" content="http://delphinelemire.fr/images/logo.png" />
+  
+		<link href='https://fonts.googleapis.com/css?family=Gabriela' rel='stylesheet' type='text/css'>
+ 		<link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css" />
+       	<link rel="stylesheet" href="css/mycv.css" />
+        <link rel="icon" type="image/png" href="/images/logo.png" />
+       
+        <title>Delphine Lemire - Développeuse </title>
+	</head>
+	<body>   
+		<nav class="navbar navbar-inverse navbar-fixed-top navbar-default" role="navigation">
+		  <div class="container-fluid">
+			<div class="navbar-header">
+		      <button type="button" class="navbar-toggle " data-toggle="collapse" data-target="#myNavbar">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>                        
+		      </button>
+		      <a class="navbar-brand" href="#">Delphine Lemire</a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="myNavbar">
+		      <ul class="nav navbar-nav">
+		        <li class="active"><a data-toggle="collapse" data-target=".navbar-collapse" href="#">Profil</a></li>
+		        <li><a data-toggle="collapse" data-target=".navbar-collapse" href="#parcours">Parcours</a></li>
+		        <li><a data-toggle="collapse" data-target=".navbar-collapse" href="#technos">Technos</a></li>
+		      	<li><a data-toggle="collapse" data-target=".navbar-collapse" href="#contact">Contact</a></li>
+		      </ul>
+		    </div>
+		 </div>
+		</nav>
+		<div class="container-fluid" >		
+		 	<div class="page-header">
+		 	 <div class="container">
+		 	    <h1> <img src="images/logo.png" class="img-responsive" width="300px" alt="logo" />Delphine Lemire, analyste développeur</h1>
+				<p class="great"><i>De formation scientifique, je développe des applications de gestion ou backoffice depuis 2000. 
+							A l'aise sous windows et linux, je jongle avec vos données et les mets à votre disposition sous toutes les formes.
+							Si vous représentez une startup ou une entreprise à taille très humaine utilisant ou souhaitant utiliser les technos d'aujourd'hui, pensez à Moi
+							 </i>
+				</p>
+				<div class="signature great"><i>Delphine Lemire</i></div>
+			 </div>
+			</div>
+		</div>
+		<noscript>
+			<div class="alert alert-info" role="alert">
+				  Le JavaScript n'est pas activé sur votre navigateur. 
+				  L'accès aux fonctionnalités de ce site sera limitée.
+				  ...
+			</div>
+		</noscript>
+		<span id="parcours"><br></span>
+		<div  class="container-fluid color-background" >
+		  <div  class="container"><h1>Mon Parcours</h1><p class="great"> Interviews, animation de réunion, développement, test, formation</p>
+			<div class="row">
+				<div class="col-md-1 text-center  ">
+	    			<span class="glyphicon-class label label-default great">2016</span>
+				</div>
+				<div class="col-md-6">
+			   		<h4>Analyste Développeur</h4>
+					   		<p><i>Chez Vous?/Client final</i></p>
+							<ul>
+								<li>Entreprise à taille humaine, STARTUP?</li>
+								<li>Développement PHP/PYTHON/JAVA ou Node.js sous MySQL/PostgreSQL ou  Bdd NoSQL?  </li>
+								<li>Sous Windows, Linux, autre?</li>
+								<li>Encadré par Symfony2, Django, Odoo?</li>
+								<li>En mode SCRUM, avec GIT, Redmine, Jenkins, Eclipse?</li>
+							</ul>
+			 	</div>
+			</div>
+			<div class="row">
+				<div class="col-md-1  text-center ">
+	    					<span class="glyphicon-class label label-default great ">2014</span>
+				</div>
+				<div class="col-md-6">
+				   <h4>Analyste Développeur</h4>
+						   <p><i>Delphine Lemire/SCOP276 (76)</i></p>
+							<ul>
+								<li>Développement d'un outil de suivi de stock Symfony2 (PHP/Mysql)</li>
+								<li>Laboratoire de serveurs sous Ubuntu</li>
+								<li>Autoformation symfony2 (PHP/Mysql) et Odoo (Python/Postgresql)
+							</ul>
+				</div>
+			</div>
+			<div class="row">
+	  		  	<div class="col-md-1  text-center ">
+	    					<span class="glyphicon-class label label-default great ">2011</span>
+				</div>
+				<div class="col-md-6">
+					   		<h4>Analyste Développeur</h4>
+					   			<p><i>Magic Recycle (75/Client final)</i></p>
+								<ul>
+									<li>Développement web service entre site ecommerce sous DRUPAL6 (PHP/MySQL) et
+									 l'outil de gestion OpenERP6 (Python/PostgreSQL)</li>
+									<li>Développement modules spécifiques OpenERP</li>
+								</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-1  text-center ">
+	    					<span class="glyphicon-class label label-default great ">2010</span>
+				</div>
+				<div class="col-md-6">
+						   <h4>Analyste Développeur</h4>
+						   <p><i>Renault Tech (27/Client final)</i></p>
+							<ul>
+								<li>Développement état sous WDETAT, intégralement programmé (Windev/SQL SERVER)</li>
+								<li>Développement maquette de creation de plan (Excel/VBA)</li>
+							</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-1  text-center ">
+	    					<span class="glyphicon-class label label-default great ">2008</span>
+				</div>
+				<div class="col-md-6">
+						   <h4>Analyste Développeur</h4>
+						   <p><i>Castelis (94)</i></p>
+							<ul>
+								<li>Développement outil de gestion d'entrepôt (Windev/SQLServer)</li>
+								<li>Développement outil de gestion de recettes alimentaires (PHP/Mysql)</li>
+							</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-1  text-center ">
+	    					<span class="glyphicon-class label label-default great ">2006</span>
+				</div>
+				<div class="col-md-6">
+							   <h4>Analyste Développeur</h4>
+							   <p><i>Duhamel Logisque(27/Client final)</i></p>
+								<ul>
+									<li>Développement outil de gestion d'entrepôt classé SEVESO (DELPHI/ORACLE)</li>
+								</ul>
+				</div>
+			</div>
+			<div class="row">
+			 		<div class="col-md-1  text-center ">
+		    					<span class="glyphicon-class label label-default great ">2004</span>
+							</div>
+			<div class="col-md-6">
+							   <h4>Analyste Développeur</h4>
+							   <p><i>Tchad Cameroun Logistique (Cameroun/Tchad/Client final)</i></p>
+								<ul>
+									<li>Développement outil de suivi de stock (Visual Basic/SQL SERVER)</li>
+								</ul>
+							</div>
+			</div>
+			<div class="row">
+				<div class="col-md-1  text-center ">
+		    					<span class="glyphicon-class label label-default great ">2000</span>
+				</div>
+				<div class="col-md-6">
+							   <h4>Analyste Développeur</h4>
+							   <p><i>Areva (27/Client final)</i></p>
+								<ul>
+									<li>Développement outil de gestion (Excel/Access)</li>
+								</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<h1 id="technos">Technos</h1>
+		<div>
+			<div class="left sprite-vignettes-access"></div>
+			<div class="left sprite-vignettes-mysql"></div>
+			<div class="left sprite-vignettes-postgresql"></div>
+			<div class="left sprite-vignettes-oracle"></div>
+			<div class="left sprite-vignettes-jasperreport"></div>
+			<div class="left sprite-vignettes-drupal"></div>
+			<div class="left sprite-vignettes-symfony"></div>
+			<div class="left sprite-vignettes-php"></div>
+			<div class="left sprite-vignettes-excel"></div>
+			<div class="left sprite-vignettes-python"></div>	
+			<div class="left sprite-vignettes-debian"></div>
+			<div class="left sprite-vignettes-powerpoint"></div>
+			<div class="left sprite-vignettes-ubuntu"></div>	
+			<div class="left sprite-vignettes-redmine"></div>	
+  			<div class="left sprite-vignettes-jenkins"></div>						
+			<div class="left sprite-vignettes-wamp"></div>
+			<div class="left sprite-vignettes-odoo"></div>	
+			<div class="left sprite-vignettes-eclipse"></div>					
+		</div>
+	</div>
+	<div id="contact" class="container-fluid color-background">  
+		<div class="container">  
+		<h1>Contact</h1>       
+		    	<ul class="nopuce great ">
+		    		<li><a href="tel:+33609700569">
+			 			<span class="glyphicon glyphicon-phone" aria-hidden="true"> </span>
+			 			 0609700469
+			 		</a> </li>
+			 		<li><a href="mailto:delphine.lemire@gmail.com
+						?subject=Prise de contact via le site delphinelemire.fr
+						&body=Bonjour"><span class="glyphicon glyphicon-envelope" aria-hidden="true"> </span>
+						delphine.lemire@gmail.com
+					</a></li>
+			 		<li><a href="docs/CV-DelphineLemire-20160606.pdf"><span class="glyphicon glyphicon-cloud-download" aria-hidden="true">  </span>
+						  <i>Curriculum Vitae<br/> en PDF</i></a>	</li>
+		    	</ul> 
+		</div>
+	</div>
+	<footer class="center">
+		    <p>Copyright Delphine Lemire - Tous droits réservés<br />
+		13, route de Nassandres - 27470 SERQUIGNY
+		   </p>
+	</footer>
+	   <script type="text/javascript" src="js/jquery-1.12.4.min.js"></script>
+	   <script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</body>
+</html>
